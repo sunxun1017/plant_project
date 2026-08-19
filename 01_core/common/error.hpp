cc2b6b29534a9184a8ba87ff1515c624427b91b1
sunxun <sx2728977548.com>
2026-08-19 23:04:22 +0800

    chore(project): add initial module scaffold

---
+++ 01_core/common/error.hpp
@@ -0,0 +1,8 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2026-08-19 22:14:42
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2026-08-19 22:23:39
+ * @FilePath: /plant_project/01_core/common/error.hpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
